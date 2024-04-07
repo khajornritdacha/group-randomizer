e2778c1d777dfcb838d7c82a89686cf9a3e9dd45
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-04-07 21:32:52 +0700

    edit readme

--- src/components/GroupInformation.svelte
+++ src/components/GroupInformation.svelte
@@ -14,15 +14,15 @@
 		type="number"
 		bind:value={$data_store.length}
 		disabled
-		class="p-2 ml-2 rounded grow"
+		class="p-2 ml-2 rounded grow disabled: bg-gray-100"
 		id="n_member"
 	/>
 	<label class="py-3 text-xl flex font-bold" for="n_group">Number of groups :</label>
 	<select
 		bind:value={group_cnt}
-		class="p-2 ml-2 rounded grow"
-		id="n_group"
 		disabled={!$data_store.length}
+		class="p-2 ml-2 rounded grow disabled: bg-gray-100"
+		id="n_group"
 	>
 		{#each PRIMES as prime}
 			<option value={prime}>{prime}</option>
@@ -35,7 +35,7 @@
 		disabled={!group_cnt}
 		min="1"
 		max={group_cnt - 1}
-		class="p-2 ml-2 rounded grow"
+		class="p-2 ml-2 rounded grow disabled: bg-gray-100"
 		id="n_day"
 	/>
 	<div class="flex justify-between gap-4 items-center">
@@ -46,12 +46,13 @@
 			class="p-2 ml-2 rounded grow mt-1"
 			id="control_sheet"
 		/>
-		<label class="py-3 text-xl flex font-bold" for="control_sheet">Don't generate template</label>
+		<label class="py-3 text-xl flex font-bold" for="control_sheet">Download minimal version</label>
 	</div>
 	<div class="flex justify-between gap-4 items-center">
 		<input
 			type="checkbox"
 			bind:checked={enableForbiddenPairs}
+			disabled={!group_cnt}
 			class="p-2 ml-2 rounded grow mt-1"
 			id="control_sheet"
 		/>
